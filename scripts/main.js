818a12b64b1b5c14edf259fa20959b283c4d559c
Corey Waple <corey.waple@gmail.com>
2017-05-22 00:54:42 +0200

    Reset now works and the event listener works after reset again to change the grid color.

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,28 @@ gridSize = 16;
 $(document).ready(function() {
 	createGrid(gridSize, gridSize);
 
-	$('.gridSquare').hover(function() {
+	$(document).on("mouseleave", ".gridSquare", function() {
 		$(this).addClass("gridSquareClicked");
 	});
+
+	/*$(".gridSquare").live("hover", function() { 
+		$(this).addClass("gridSquareClicked");
+	});*/
 
 	$('#button').click(function() { 
 		gridSize = prompt("Please enter size", gridSize); 
 		if (gridSize !== null) {
+			$('div.gridSquare').removeClass('gridSquareClicked');
 			$('#container').html(""); 
 			createGrid(gridSize, gridSize); 
 		}
 	});
+
+	$(".gridSquare").hover(function() {
+		$(this).addClass("gridSquareClicked");
+	});
 }); 
+
 
 function createGrid(size, remains) {
 	if (remains === 0) {
