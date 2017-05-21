151d76df36bfd477c63010f0602afadd92ebfefb
Corey Waple <corey.waple@gmail.com>
2017-05-21 18:28:19 +0200

    Grid constructed. Grids change colors when clicked and a different color for hovers. Reset button would be nice. However toggeleClass works for change them back.

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+gridSize = 16; 
+
+$(document).ready(function() {
+	createGrid(gridSize, gridSize);
+
+	$('.gridSquare').click(function() {
+		$(this).toggleClass("gridSquareClicked");
+	});
+
+	$('#button').click(function() { 
+
+	});
+}); 
+
+function createGrid(size, remains) {
+	if (remains === 0) {
+		return; 
+	} else {
+		for (var i = 0; i < 16; i++) {
+			$('#container').append("<div class='gridSquare'></div>"); 
+		}
+		$('#container').append("<br />");
+		remains = remains - 1; 
+		createGrid(size, remains); 
+	}
+
+function reset(gridSize, gridSize) {
+	if
+}
+
+}
